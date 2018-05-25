e0efac8aa54fffe0d635d1c6e5e3633457752ff4
Fabian Jahr <fabian.jahr@whu.edu>
2018-05-25 23:50:39 +0200

    use exclamation mark for methods without a return status touple

--- lib/satoshi/util.ex
+++ lib/satoshi/util.ex
@@ -8,12 +8,12 @@ defmodule Satoshi.Util do
 
   ## Examples
 
-      iex> Satoshi.Util.double_sha256('Hello World')
+      iex> Satoshi.Util.double_sha256!('Hello World')
       <<66, 168, 115, 172, 58, 189, 2, 18, 45, 39, 232, 4, 134, 198, 250, 30, 247,
       134, 148, 232, 80, 95, 206, 201, 203, 204, 138, 119, 40, 186, 137, 73>>
   """
-  @spec double_sha256(binary) :: binary
-  def double_sha256(value) do
+  @spec double_sha256!(binary) :: binary
+  def double_sha256!(value) do
     :crypto.hash(:sha256, :crypto.hash(:sha256, value))
   end
 
