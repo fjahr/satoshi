31ea0181f2f645b6aa540fcaa1c8d0da85eabd65
Fabian Jahr <fabian.jahr@whu.edu>
2018-05-30 01:11:56 +0200

    some refactoring using pipe operator for readability

--- lib/satoshi/own/field_element.ex
+++ lib/satoshi/own/field_element.ex
@@ -7,28 +7,35 @@ defmodule Satoshi.Own.FieldElement do
   defstruct [:value, :prime]
 
   def add(%{value: val1, prime: prime}, %{value: val2, prime: prime}) do
-    new_value = rem((val1 + val2), prime)
+    new_value = val1 + val2
+      |> rem(prime)
 
     %__MODULE__{value: new_value, prime: prime}
   end
   def add(_, _), do: raise ArgumentError
 
   def sub(%{value: val1, prime: prime}, %{value: val2, prime: prime}) do
-    new_value = abs(rem((val1 - val2), prime))
+    new_value = val1 - val2
+      |> rem(prime)
+      |> abs()
 
     %__MODULE__{value: new_value, prime: prime}
   end
   def sub(_, _), do: raise ArgumentError
 
   def mul(%{value: val1, prime: prime}, %{value: val2, prime: prime}) do
-    new_value = abs(rem((val1 * val2), prime))
+    new_value = val1 * val2
+      |> rem(prime)
+      |> abs()
 
     %__MODULE__{value: new_value, prime: prime}
   end
   def mul(_, _), do: raise ArgumentError
 
   def rmul(%{value: val1, prime: prime}, val2) when is_integer(val2) do
-    new_value = abs(rem((val1 * val2), prime))
+    new_value = val1 * val2
+      |> rem(prime)
+      |> abs()
 
     %__MODULE__{value: new_value, prime: prime}
   end
