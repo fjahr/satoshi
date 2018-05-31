da076ba13de86420c0fa4415aa6ef9c79a7cd280
Fabian Jahr <fabian.jahr@whu.edu>
2018-05-31 21:20:54 +0200

    add point addition

--- lib/satoshi/own/point.ex
+++ lib/satoshi/own/point.ex
@@ -13,6 +13,14 @@ defmodule Satoshi.Own.Point do
   @enforce_keys [:x, :y, :a, :b]
   defstruct [:x, :y, :a, :b]
 
+  def new(x: x, y: y, a: a, b: b, prime: prime) do
+    fe_x = FieldElement.new(value: x, prime: prime)
+    fe_y = FieldElement.new(value: y, prime: prime)
+
+    %__MODULE__{x: fe_x, y: fe_y, a: a, b: b}
+  end
+  def new(x: x, y: y, a: a, b: b), do: %__MODULE__{x: x, y: y, a: a, b: b}
+
   def s256point(raw_x, raw_y) do
     x = FieldElement.new_s256(value: raw_x)
     y = FieldElement.new_s256(value: raw_y)
@@ -22,13 +30,54 @@ defmodule Satoshi.Own.Point do
     if point_on_s256?(p) do
       p
     else
-      raise ArgumentError
+      raise ArgumentError, "Point is not on secp256k1"
     end
   end
 
   def g() do
     s256point(@gx, @gy)
   end
+
+  def eq(%__MODULE__{x: x, y: y, a: a, b: b}, %__MODULE__{x: x, y: y, a: a, b: b}) do
+    true
+  end
+  def eq(_, _), do: false
+
+  def add(%__MODULE__{x: %{value: nil}, y: %{value: nil}, a: _, b: _}, p2), do: p2
+  def add(p1, %__MODULE__{x: %{value: nil}, y: %{value: nil}, a: _, b: _}), do: p1
+  def add(%__MODULE__{x: x, y: y, a: a, b: b}, %__MODULE__{x: x, y: y, a: a, b: b}) do
+    s = FieldElement.pow(x, 2)
+        |> FieldElement.rmul(3)
+        |> FieldElement.add(a)
+        |> FieldElement.div(FieldElement.rmul(y, 2))
+
+    new_x = FieldElement.pow(s, 2)
+        |> FieldElement.sub(FieldElement.rmul(x, 2))
+
+    new_y = FieldElement.sub(x, new_x)
+            |> FieldElement.mul(s)
+            |> FieldElement.sub(y)
+
+    new(x: new_x, y: new_y, a: a, b: b)
+  end
+  def add(%__MODULE__{x: x, y: y1, a: a, b: b}, %__MODULE__{x: x, y: y2, a: a, b: b}) do
+    new(x: nil, y: nil, a: a, b: b, prime: x.prime)
+  end
+  def add(%__MODULE__{x: x1, y: y1, a: a, b: b}, %__MODULE__{x: x2, y: y2, a: a, b: b}) do
+    s = FieldElement.sub(y2, y1)
+        |> FieldElement.div(FieldElement.sub(x2, x1))
+
+    new_x = FieldElement.pow(s, 2)
+        |> FieldElement.sub(x1)
+        |> FieldElement.sub(x2)
+
+    new_y = FieldElement.sub(x1, new_x)
+            |> FieldElement.mul(s)
+            |> FieldElement.sub(y1)
+
+    new(x: new_x, y: new_y, a: a, b: b)
+  end
+  def add(_, _), do: raise ArgumentError, "Point addition not possible for these parameters"
 
   defp point_on_s256?(p) do
     left_side = FieldElement.pow(p.y, 2)
--- test/satoshi/own/point_test.exs
+++ test/satoshi/own/point_test.exs
@@ -24,4 +24,30 @@ defmodule Satoshi.Own.PointTest do
       Point.s256point(24, 220)
     end
   end
+
+  test "eq test for equality in points" do
+    p1 = Point.new(x: 2, y: 5, a: 5, b: 7, prime: 97)
+    p2 = Point.new(x: 2, y: 5, a: 5, b: 7, prime: 97)
+    p3 = Point.new(x: 2, y: -5, a: 5, b: 7, prime: 97)
+
+    assert Point.eq(p1, p2) == true
+    assert Point.eq(p2, p3) == false
+  end
+
+  test "add/2 is performing point addition" do
+    a = Point.new(x: nil, y: nil, a: 5, b: 7, prime: 223)
+    b = Point.new(x: 2, y: 5, a: 5, b: 7, prime: 223)
+    c = Point.new(x: 2, y: 6, a: 5, b: 7, prime: 223)
+
+    assert Point.add(a, b) == b
+    assert Point.add(b, a) == b
+    assert Point.add(b, c) == a
+
+    d = Point.new(x: 3, y: 7, a: 5, b: 7, prime: 223)
+    e = Point.new(x: 1, y: 1, a: 5, b: 7, prime: 223)
+    assert Point.add(d, e) == Point.new(x: 5, y: 210, a: 5, b: 7, prime: 223)
+
+    f = Point.new(x: 1, y: 1, a: 5, b: 7, prime: 223)
+    assert Point.add(f, f) == Point.new(x: 14, y: 170, a: 5, b: 7, prime: 223)
+  end
 end
